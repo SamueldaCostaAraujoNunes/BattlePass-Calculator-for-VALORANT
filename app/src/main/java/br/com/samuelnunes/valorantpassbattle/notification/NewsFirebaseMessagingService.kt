2562be0fd676c86c7e6ee7b76e55222c541ea02a
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2021-06-07 20:58:18 -0300

    Ativei o FCM

---
+++ app/src/main/java/br/com/samuelnunes/valorantpassbattle/notification/NewsFirebaseMessagingService.kt
@@ -0,0 +1,8 @@
+package br.com.samuelnunes.valorantpassbattle.notification
+
+import android.annotation.SuppressLint
+import com.google.firebase.messaging.FirebaseMessagingService
+
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class NewsFirebaseMessagingService : FirebaseMessagingService()
