9308f972a00ecebb8be8aba10d5246a09775cc4a
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2021-06-06 22:31:38 -0300

    As imagens agr são salvas e recuperadas do storage

--- app/src/main/java/br/com/samuelnunes/valorantpassbattle/extensions/ImageViewExtension.kt
+++ app/src/main/java/br/com/samuelnunes/valorantpassbattle/extensions/ImageViewExtension.kt
@@ -5,17 +5,22 @@ import android.graphics.drawable.ColorDrawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import br.com.samuelnunes.valorantpassbattle.R
-import com.bumptech.glide.Glide
+import br.com.samuelnunes.valorantpassbattle.util.GlideApp
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+
 
 @BindingAdapter("imageUrl")
 fun ImageView.loadImageURL(url: String?) {
     if (url != null) {
+        val gsReference: StorageReference = Firebase.storage.getReference(url)
         val drawableTransitionOptions = DrawableTransitionOptions().crossFade(
             DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
         )
-        Glide.with(context).load(url)
+        GlideApp.with(context).load(gsReference)
             .placeholder(ColorDrawable(Color.TRANSPARENT))
             .error(R.drawable.img_error)
             .transition(drawableTransitionOptions)
@@ -23,4 +28,3 @@ fun ImageView.loadImageURL(url: String?) {
             .into(this)
     }
 }
-
---
+++ app/src/main/java/br/com/samuelnunes/valorantpassbattle/util/GlideWithFirebaseStorage.kt
@@ -0,0 +1,27 @@
+package br.com.samuelnunes.valorantpassbattle.util
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.firebase.ui.storage.images.FirebaseImageLoader
+import com.google.firebase.storage.StorageReference
+import java.io.InputStream
+
+@GlideModule
+class GlideWithFirebaseStorage : AppGlideModule() {
+
+    override fun registerComponents(
+        context: Context,
+        glide: Glide,
+        registry: Registry
+    ) {
+        super.registerComponents(context, glide, registry)
+        registry.append(
+            StorageReference::class.java, InputStream::class.java,
+            FirebaseImageLoader.Factory()
+        )
+
+    }
+}
