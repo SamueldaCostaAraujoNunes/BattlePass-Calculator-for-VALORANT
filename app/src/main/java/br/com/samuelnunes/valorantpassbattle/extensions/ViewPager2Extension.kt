7f3c72bf377a40dbb815bc8f767d7b75e72f1b20
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2021-06-11 22:05:45 -0300

    Finalmente, o ultimo bug arrumado! ...Espero

--- app/src/main/java/br/com/samuelnunes/valorantpassbattle/extensions/ViewPager2Extension.kt
+++ app/src/main/java/br/com/samuelnunes/valorantpassbattle/extensions/ViewPager2Extension.kt
@@ -1,15 +1,10 @@
 package br.com.samuelnunes.valorantpassbattle.extensions
 
-import android.animation.ValueAnimator
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.BindingAdapter
 import androidx.viewpager2.widget.ViewPager2
 import br.com.samuelnunes.valorantpassbattle.ui.view.adapter.FragmentSliderAdapter
 import br.com.samuelnunes.valorantpassbattle.ui.view.adapter.ImageSliderAdapter
-import br.com.samuelnunes.valorantpassbattle.ui.view.viewsCustom.CardModule
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
-import timber.log.Timber
 
 @BindingAdapter("imagesURl", "indicator")
 fun ViewPager2.imagesURl(imagesURl: List<String>?, indicator: DotsIndicator?) {
@@ -25,53 +20,5 @@ fun ViewPager2.imagesURl(imagesURl: List<String>?, indicator: DotsIndicator?) {
 fun ViewPager2.setAdapterSlider(mAdapter: FragmentSliderAdapter, adaptiveHeight: Boolean) {
     adapter = mAdapter
 
-    registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-        override fun onPageScrolled(
-            position: Int,
-            positionOffset: Float,
-            positionOffsetPixels: Int
-        ) {
-            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-            val item = mAdapter.getItem(position)
-            item.title.observe(item, {
-                CardModule.parentIsCardmodule(item.view)?.title(it)
-            })
-        }
 
-        override fun onPageSelected(position: Int) {
-            super.onPageSelected(position)
-            try {
-                val item = mAdapter.getItem(position)
-                val v: View? = item.view
-                if (adaptiveHeight && v != null) {
-                    changeDimensLayoutAnimation(v)
-                }
-            } catch (e: Exception) {
-                Timber.e(e)
-            }
-
-        }
-
-        private fun changeDimensLayoutAnimation(v: View) {
-            v.post {
-                val wMeasureSpec =
-                    View.MeasureSpec.makeMeasureSpec(v.width, View.MeasureSpec.EXACTLY)
-                val hMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-                v.measure(wMeasureSpec, hMeasureSpec)
-                try {
-                    val anim = ValueAnimator.ofInt(layoutParams.height, v.measuredHeight)
-                    anim.addUpdateListener {
-                        val height = it.animatedValue as Int
-                        layoutParams.height = height
-                    }
-                    anim.duration = 210
-                    anim.start()
-                } catch (e: IllegalStateException) {
-                    layoutParams = (layoutParams as ViewGroup.LayoutParams)
-                        .also { lp -> lp.height = v.measuredHeight }
-                }
-            }
-        }
-
-    })
 }
--- app/src/main/java/br/com/samuelnunes/valorantpassbattle/ui/view/viewsCustom/PageSliderFragment.kt
+++ app/src/main/java/br/com/samuelnunes/valorantpassbattle/ui/view/viewsCustom/PageSliderFragment.kt
@@ -1,15 +1,16 @@
 package br.com.samuelnunes.valorantpassbattle.ui.view.viewsCustom
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.viewpager2.widget.ViewPager2
 import br.com.samuelnunes.valorantpassbattle.databinding.FragmentPagerSliderBinding
 import br.com.samuelnunes.valorantpassbattle.extensions.setAdapterSlider
 import br.com.samuelnunes.valorantpassbattle.ui.view.adapter.FragmentSliderAdapter
+import timber.log.Timber
 
 abstract class PagerSliderFragment(
     private var fragments: List<FragmentWithTitle> = ArrayList(),
@@ -29,20 +30,51 @@ abstract class PagerSliderFragment(
 
         binding.lifecycleOwner = viewLifecycleOwner
         binding.pager.setAdapterSlider(adapter, adaptiveHeight)
+        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                try {
+                    val item = adapter.getItem(position)
+                    binding.cardModule.title(getString(item.titleResId))
+                    val v: View? = item.view
+                    if (adaptiveHeight && v != null) {
+                        changeDimensLayoutAnimation(v, binding.pager)
+                    }
+                } catch (e: Exception) {
+                    Timber.e(e)
+                }
+
+            }
+
+        })
         binding.dotsIndicator.setViewPager2(binding.pager)
-//        binding.dotsIndicator.dotsClickable = false
+        binding.dotsIndicator.dotsClickable = false
         return binding.root
+    }
+
+    private fun changeDimensLayoutAnimation(v: View, parent: ViewPager2) {
+        v.post {
+            val wMeasureSpec =
+                View.MeasureSpec.makeMeasureSpec(v.width, View.MeasureSpec.EXACTLY)
+            val hMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            v.measure(wMeasureSpec, hMeasureSpec)
+            try {
+                val anim = ValueAnimator.ofInt(parent.layoutParams.height, v.measuredHeight)
+                anim.addUpdateListener {
+                    val height = it.animatedValue as Int
+                    parent.layoutParams.height = height
+                }
+                anim.duration = 210
+                anim.start()
+            } catch (e: IllegalStateException) {
+                parent.layoutParams = (parent.layoutParams as ViewGroup.LayoutParams)
+                    .also { lp -> lp.height = v.measuredHeight }
+            }
+        }
     }
 }
 
 abstract class FragmentWithTitle : Fragment() {
-    protected val _title: MutableLiveData<String> = MutableLiveData()
-    val title: LiveData<String> = _title
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        _title.postValue(getString(titleResId))
-    }
-
     abstract val titleResId: Int
 }
